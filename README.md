1def0e8b61a34c5d41d66ef6ac49d83942d16b91
Thomas Duchatelle <duchatelle.thomas@gmail.com>
2015-11-07 13:03:17 +0000

    Create Maven structure project based on spring boot.

---
+++ README.md
@@ -0,0 +1,15 @@
+Checkout Calculator
+===================
+
+Compile and install
+-------------------
+
+This is a simple _Maven_ project. Just run in root directory (where this file is):
+
+    mvn clean install
+
+To use it, run after compilation:
+
+    java -jar target/checkout-0.1-SNAPSHOT
+
+You'll have an invite command to choose what items you want to take.
---
+++ src/main/java/fr/dush/checkout/CheckoutCalculator.java
@@ -0,0 +1,13 @@
+package fr.dush.checkout;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+/** Spring boot entry point - main configuration */
+@SpringBootApplication
+public class CheckoutCalculator {
+
+    public static void main(String[] args) {
+        SpringApplication.run(CheckoutCalculator.class, args);
+    }
+}
